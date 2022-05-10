612bcbb40c12f64fb3f34a2f2b6723876f4dcfd6
AEON-MERX <103615875+AEON-MERX@users.noreply.github.com>
2022-05-09 22:24:22 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ function validar(){
     window.location = "https://aeon-merx.github.io/AEON-MERX/bgzdbndgljblnjdzgnlbjnldg.pdf"; 
     }
       
-    else if( cursos == "Programacion" || cursos=="programacion" || cursos=="PROGRAMACION" || cursos=="Programación"){
+    else if( cursos == "Tarot" || cursos=="tarot" || cursos=="TAROT" || cursos=="Programación"){
     window.location = "https://aeon-merx.github.io/AEON-MERX/libro_thoth_espanol.pdf"; 
     }
     else
